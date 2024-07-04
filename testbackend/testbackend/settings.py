b22b52a2de1f2ea000723b187cacaaf411243581
Ghassen Bougacha <ghassen.bougacha@gmail.com>
2024-07-04 08:57:35 +0100

    add libraries

--- testbackend/testbackend/settings.py
+++ testbackend/testbackend/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from corsheaders.defaults import default_headers
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wondu-()=)dydup23ts8%uzdb^o+p)(1*w04#g4*vq$uk2wcm#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "off") == "on"
+PRODUCTION = os.environ.get("PRODUCTION", "off") == "on"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
